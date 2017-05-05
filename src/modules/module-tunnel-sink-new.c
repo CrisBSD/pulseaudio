f7241c0d2994cd4204e6fde4ba6be54a572412df
Vadim Troshchinskiy <vtroshchinskiy@qindel.com>
2017-05-05 12:56:08 +0200

    Improved Opus audio compression
    This is a change based on the Opus audio compression patch by Gavin
    Darkglider found at https://bugs.freedesktop.org/show_bug.cgi?id=56993.
    
    The following changes have been made:
    
    * Adapt the code to the latest PA version.
    * Add the ability to configure more Opus parameters.
    * Make most parameters configurable during the execution. This means
      that for instance the bitrate can be varied at runtime as needed.
    * Opus specific code has been moved out from tunnel-sink into
      transcode.c
    
    A few Opus features have been left unexposed for the following reasons:
    
    * OPUS_SET_FORCE_CHANNELS - This does not seem to make sense for an end
      user to change.
    * OPUS_GET_LOOKAHEAD - Latency management is best left to pulseaudio
    * OPUS_SET_INBAND_FEC - The implementation is over TCP, so FEC doesn't
      make sense.
    * OPUS_SET_PACKET_LOSS_PERC - Same as above
    * OPUS_SET_EXPERT_FRAME_DURATION - This might be worth looking into,
      but it looks like something for PA to use internally, rather than
      something to expose to the user.
    * OPUS_SET_PREDICTION_DISABLED - Also looks like something for
      internal use.

--- src/modules/module-tunnel-sink-new.c
+++ src/modules/module-tunnel-sink-new.c
@@ -73,6 +73,7 @@ static void stream_set_buffer_attr_cb(pa_stream *stream, int success, void *user
 static void context_state_cb(pa_context *c, void *userdata);
 static void sink_update_requested_latency_cb(pa_sink *s);
 
+
 struct userdata {
     pa_module *module;
     pa_sink *sink;
@@ -93,6 +94,7 @@ struct userdata {
     char *remote_sink_name;
 
     pa_transcode transcode;
+    pa_hook_slot *sink_changed_slot;
 };
 
 static const char* const valid_modargs[] = {
@@ -111,6 +113,8 @@ static const char* const valid_modargs[] = {
    /* "reconnect", reconnect if server comes back again - unimplemented */
     NULL,
 };
+
+static pa_hook_result_t sink_changed_cb(pa_core *c, pa_object *o, struct userdata *u);
 
 static void cork_stream(struct userdata *u, bool cork) {
     pa_operation *operation;
@@ -154,6 +158,10 @@ static pa_proplist* tunnel_new_proplist(struct userdata *u) {
 static void thread_func(void *userdata) {
     struct userdata *u = userdata;
     pa_proplist *proplist;
+    unsigned int frame_size;
+
+
+
     pa_assert(u);
 
     pa_log_debug("Thread starting up");
@@ -184,6 +192,13 @@ static void thread_func(void *userdata) {
         goto fail;
     }
 
+    // TODO: fix this, probably move to the initialization code
+    u->transcode.proplist = u->sink->proplist;
+
+    // The parameter is limited internally and can't be negative
+    if ( u->transcode.encoding != -1 )
+        frame_size = (unsigned int)pa_transcode_get_param(&u->transcode, PA_PROP_COMPRESSION_FRAME_SIZE);
+
     for (;;) {
         int ret;
 
@@ -204,10 +219,10 @@ static void thread_func(void *userdata) {
                          size_t nbBytes;
                          unsigned char *cbits;
 
-                         pa_sink_render_full(u->sink, u->transcode.frame_size*u->transcode.channels*u->transcode.sample_size, &memchunk);
+                         pa_sink_render_full(u->sink, frame_size*u->transcode.channels*u->transcode.sample_size, &memchunk);
 
                          pa_assert(memchunk.length > 0);
-                         pa_assert(memchunk.length >=  u->transcode.frame_size*u->transcode.channels);
+                         pa_assert(memchunk.length >=  frame_size*u->transcode.channels);
 
 
                          pa_log_debug("received memchunk length: %zu bytes", memchunk.length );
@@ -223,7 +238,7 @@ static void thread_func(void *userdata) {
                                                nbBytes,
                                                NULL,     /**< A cleanup routine for the data or NULL to request an internal copy */
                                                0,        /** offset */
-                                              PA_SEEK_RELATIVE, u->transcode.frame_size*u->transcode.channels*u->transcode.sample_size);
+                                              PA_SEEK_RELATIVE, frame_size*u->transcode.channels*u->transcode.sample_size);
                          pa_memblock_release(memchunk.memblock);
                          pa_memblock_unref(memchunk.memblock);
                          if(nbBytes > 0) free(cbits);
@@ -577,15 +592,16 @@ int pa__init(pa_module *m) {
         pa_log("compression activated");
         memset(&u->transcode, 0, sizeof(pa_transcode));
 
-        if(strcmp(compression, "opus") == 0 && pa_transcode_supported(PA_ENCODING_OPUS)){
+        if(pa_transcode_supported_byname(compression)){
 
-             compression = pa_modargs_get_value(ma, "compression-frame_size", NULL);
-             if(compression) u->transcode.frame_size = atoi(compression);
-             compression = pa_modargs_get_value(ma, "compression-bitrate", NULL);
-             if(compression) u->transcode.bitrate = atoi(compression);
+		pa_encoding_t encoding = pa_transcode_encoding_byname(compression);
 
-             pa_transcode_init(&u->transcode, PA_ENCODING_OPUS, PA_TRANSCODE_ENCODER, NULL, &ss);
+//             pa_proplist_sets(sink_data.proplist, "compression.algorithm", "opus");
+//             pa_transcode_copy_arguments(&u->transcode, ma, sink_data.proplist);
 
+
+             pa_transcode_init(&u->transcode, encoding, PA_TRANSCODE_ENCODER, NULL, &ss, ma, sink_data.proplist);
+	     u->sink_changed_slot = pa_hook_connect(&m->core->hooks[PA_CORE_HOOK_SINK_PROPLIST_CHANGED], PA_HOOK_LATE, (pa_hook_cb_t)sink_changed_cb, u);
          }
     }
     else u->transcode.encoding = -1;
@@ -685,4 +701,20 @@ void pa__done(pa_module *m) {
         pa_transcode_free(&u->transcode);
 
     pa_xfree(u);
+}
+
+/* Runs in PA mainloop context */
+static pa_hook_result_t sink_changed_cb(pa_core *c, pa_object *o, struct userdata *u) {
+    pa_assert(c);
+    pa_object_assert_ref(o);
+
+    // TODO: a lock is probably needed here
+    pa_mutex_lock(u->transcode.codec_mutex);
+
+    pa_transcode_verify_proplist(&u->transcode);
+    pa_transcode_update_encoder_options(&u->transcode);
+
+    pa_mutex_unlock(u->transcode.codec_mutex);
+
+    return PA_HOOK_OK;
 }
--- src/pulsecore/transcode.h
+++ src/pulsecore/transcode.h
@@ -2,35 +2,69 @@
 #define footranscodehfoo
 
 #include <pulsecore/core-format.h>
+#include <pulsecore/modargs.h>
+#include <pulsecore/proplist-util.h>
+#include <pulsecore/mutex.h>
 
+/*
+ * Bitrate and frame size are used externally, in module-tunnel-sink-new
+ * We consider these the universal parameters that are going to apply to
+ * any other codec that might be implemented in the future.
+ */
+#define PA_PROP_COMPRESSION_ALGORITHM           "compression.algorithm"
+#define PA_PROP_COMPRESSION_BITRATE             "compression.bitrate"
+#define PA_PROP_COMPRESSION_FRAME_SIZE          "compression.frame_size"
 
-
+/* The rest of the parameters are Opus specific. */
+#define PA_PROP_COMPRESSION_OPUS_COMPLEXITY     "compression.opus.complexity"
+#define PA_PROP_COMPRESSION_OPUS_MAX_BANDWIDTH  "compression.opus.max_bandwidth"
+#define PA_PROP_COMPRESSION_OPUS_SIGNAL         "compression.opus.signal"
+#define PA_PROP_COMPRESSION_OPUS_VBR            "compression.opus.vbr"
+#define PA_PROP_COMPRESSION_OPUS_VBR_CONSTRAINT "compression.opus.vbr_constraint"
+#define PA_PROP_COMPRESSION_OPUS_APPLICATION    "compression.opus.application"
+#define PA_PROP_COMPRESSION_OPUS_LSB_DEPTH      "compression.opus.lsb_depth"
+#define PA_PROP_COMPRESSION_OPUS_DTX            "compression.opus.dtx"
 
 typedef enum pa_transcode_flags {
-         PA_TRANSCODE_DECODER=(1<<0),
-         PA_TRANSCODE_ENCODER=(1<<1)
-         } pa_transcode_flags_t;
+    PA_TRANSCODE_DECODER = (1 << 0),
+    PA_TRANSCODE_ENCODER = (1 << 1)
+} pa_transcode_flags_t;
 
 typedef struct pa_transcode {
- int32_t encoding;
- uint8_t channels;
- uint32_t frame_size;
- uint32_t max_frame_size;
- uint32_t sample_size;
- uint32_t rate;
- uint32_t bitrate;
- pa_transcode_flags_t flags;
- union {
-         void *decoder;
-         void *encoder;
- };
+    int32_t encoding;
+    uint8_t channels;
+    uint32_t max_frame_size;
+    uint32_t sample_size;
+    uint32_t rate;
+
+    pa_proplist *proplist;
+
+    pa_transcode_flags_t flags;
+    pa_mutex *codec_mutex;
+
+    union {
+        void *decoder;
+        void *encoder;
+    };
 } pa_transcode;
 
+void pa_transcode_copy_arguments(pa_transcode * transcode, pa_modargs * ma, pa_proplist * prop);
+void pa_transcode_verify_proplist(pa_transcode * transcode);
+void pa_transcode_update_encoder_options(pa_transcode * transcode);
+int pa_transcode_get_param(pa_transcode * transcode, const char *key);
+
 bool pa_transcode_supported(pa_encoding_t encoding);
-void pa_transcode_set_format_info(pa_transcode *transcode, pa_format_info *f);
-void pa_transcode_init(pa_transcode *transcode, pa_encoding_t encoding, pa_transcode_flags_t flags, pa_format_info *transcode_format_info, pa_sample_spec *transcode_sink_spec);
-void pa_transcode_free(pa_transcode *transcode);
-int32_t pa_transcode_encode(pa_transcode *transcode, unsigned char *pcm_input, unsigned char **compressed_output);
-int32_t pa_transcode_decode(pa_transcode *transcode, unsigned char *compressed_input, int input_length, unsigned char *pcm_output);
+bool pa_transcode_supported_byname(const char *name);
+pa_encoding_t pa_transcode_encoding_byname(const char *name);
+
+
+void pa_transcode_set_format_info(pa_transcode * transcode, pa_format_info * f);
+void pa_transcode_init(pa_transcode * transcode, pa_encoding_t encoding, pa_transcode_flags_t flags,
+                       pa_format_info * transcode_format_info, pa_sample_spec * transcode_sink_spec, pa_modargs * modargs,
+                       pa_proplist * proplist);
+void pa_transcode_free(pa_transcode * transcode);
+int32_t pa_transcode_encode(pa_transcode * transcode, unsigned char *pcm_input, unsigned char **compressed_output);
+int32_t pa_transcode_decode(pa_transcode * transcode, unsigned char *compressed_input, int input_length,
+                            unsigned char *pcm_output);
 
 #endif
